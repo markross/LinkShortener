00a1bd7dc5aad9496e8d47854c410766acf74aac
Mark Ross <mark@mross.co.uk>
2020-11-15 16:44:37 +0000

    Add server for redirecting links

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { JsonFileRepository } from './link-shortener';
+
+const port = 5000;
+ 
+const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+    const url = request.url?.substring(1);
+    const linkRepository = new JsonFileRepository();
+    
+    if (url) {
+        const redirect = linkRepository.get(url);    
+        console.log(redirect);
+        if (redirect) {
+            response.setHeader('Location', redirect);
+            response.statusCode = 301;
+        }        
+    } else {
+        response.statusCode = 404;
+    }
+
+    response.end();
+});
+ 
+server.listen(port, (error: any) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`Server listening on port ${port}`);
+    }
+});
